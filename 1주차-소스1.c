f0cd9ae7e1dcd56d09d120c54cdd699dd6d7f245
Leehwa531 <113510252+Leehwa531@users.noreply.github.com>
2022-10-06 05:48:50 +0900

    1st Week

---
+++ 1주차-소스1.c
@@ -0,0 +1,28 @@
+#define PI 3.14
+
+#include <stdio.h>
+
+float area(float r);
+float cir(float r);
+
+int main()
+{
+	float result;
+	float radius;
+	printf("원의 반지름 입력 : ");
+	scanf_s("%f", &radius);
+	result = area(radius);
+	printf("반지름 %.3f인 원의 면적 : %.3f\n", radius, result);
+	result = cir(radius);
+	printf("반지름 %.3f인 원의 둘레 : %.3f\n", radius, result);
+}
+
+float area(float r)
+{
+	return (r * r * PI);
+}
+
+float cir(float r)
+{
+	return (r * 2 * PI);
+}
