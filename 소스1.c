97014edeb35fa92cb99744251d98ef81295326ba
Leehwa531 <113510252+Leehwa531@users.noreply.github.com>
2022-10-06 05:30:47 +0900

    3rd Week

--- 소스1.c
+++ 소스1.c
@@ -1,28 +1,52 @@
-//#define PI 3.14
-//
-//#include <stdio.h>
-//
-//float area(float r);
-//float cir(float r);
-//
-//int main()
-//{
-//	float result;
-//	float radius;
-//	printf("원의 반지름 입력 : ");
-//	scanf_s("%f", &radius);
-//	result = area(radius);
-//	printf("반지름 %.3f인 원의 면적 : %.3f\n", radius, result);
-//	result = cir(radius);
-//	printf("반지름 %.3f인 원의 둘레 : %.3f\n", radius, result);
-//}
-//
-//float area(float r)
-//{
-//	return (r * r * PI);
-//}
-//
-//float cir(float r)
-//{
-//	return (r * 2 * PI);
-//}
+//6개의 데이터를 입력 받아 함수 및 배열로 합과 평균을 구하는 문제
+#define ROWSIZE 2
+#define COLSIZE 3
+
+#include <stdio.h>
+
+void input(int td[ROWSIZE][COLSIZE]);
+double sum(int td[ROWSIZE][COLSIZE]);
+void output(int td[ROWSIZE][COLSIZE]);
+
+int main()
+{
+	int td[ROWSIZE][COLSIZE];
+
+	input(td);
+	double total = sum(td);
+	printf("합: %.0f, 평균: %.3f\n", total, total /(ROWSIZE * COLSIZE));
+	output(td);
+
+	return 0;
+}
+
+void input(int td[ROWSIZE][COLSIZE])
+{
+	printf("6개의 데이터 입력 : ");
+	for (int i = 0; i < ROWSIZE; i++)
+		for (int j = 0; j < COLSIZE; j++)
+			scanf_s("%d", &td[i][j]);
+	puts("");
+}
+
+double sum(int td[ROWSIZE][COLSIZE])
+{
+	int sum = 0;
+	for (int i = 0; i < ROWSIZE; i++)
+		for (int j = 0; j < COLSIZE; j++)
+			sum += td[i][j];
+
+	return sum;
+}
+
+void output(int td[ROWSIZE][COLSIZE])
+{
+	printf("반복문 for를 이용하여 출력\n");
+	for (int i = 0; i < ROWSIZE; i++)
+	{
+		for (int j = 0; j < COLSIZE; j++)
+			printf("%d  ", td[i][j]);
+		puts("");
+	}
+	puts("");
+}
--- 소스2.c
+++ 소스2.c
@@ -1,19 +1,58 @@
+//이차원 배열 초기화를 이용한 성적 처리 실습 예제 7-5
+//#define ROWSIZE 4
+//#define COLSIZE 2
+//
 //#include <stdio.h>
 //
-//double inch(double a);
+//void input(int score[][COLSIZE], int a, int b);
+//void output(int sum, int midsum, int finalsum);
 //
 //int main()
 //{
-//	double length;
+//	int sum = 0;
+//	int midsum = 0;
+//	int finalsum = 0;
 //
-//	printf("거리를 인치로 입력하세요. -> ");
-//	scanf_s("%lf", &length);
-//	printf("입력한 %lf인치는 %lf센치미터이다.\n", length, inch(length));
+//	printf("중간 기말 성적을 입력하세요:");
+//	int score[ROWSIZE][COLSIZE] = { 0 };
+//	input(score, ROWSIZE, COLSIZE);
+//
+//	printf("       중간      기말\n");
+//	printf("----------------------------\n");
+//
+//	for (int i = 0; i < ROWSIZE; i++)
+//	{
+//		for (int j = 0; j < COLSIZE; j++)
+//		{
+//			printf("%10d", score[i][j]);
+//			sum += score[i][j];
+//			if (j == 0)
+//				midsum += score[i][j];
+//			else
+//				finalsum += score[i][j];
+//		}
+//		puts("");
+//	}
+//	printf("----------------------------\n");
+//	output(sum,midsum, finalsum);
+//
+//
 //
 //	return 0;
 //}
 //
-//double inch(double a)
+//
+//void input(int score[][COLSIZE], int a, int b)
 //{
-//	return a * 2.54;
+//	for (int i = 0; i < a; i++)
+//		for (int j = 0; j < b; j++)
+//			scanf_s("%d", &score[i][j]);
+//}
+//
+//void output(int sum,int midsum, int finalsum)
+//{
+//	printf("평균: %7.2f %7.2f\n",(double)midsum / ROWSIZE, (double)finalsum/ROWSIZE );
+//
+//	printf("\n성적의 합은 %d이고 ", sum);
+//	printf("평균은 %.2f이다. \n", (double)sum / (ROWSIZE * COLSIZE));
 //}
