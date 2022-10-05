d60fcaf4fc56819799dff0a34064f697482d68fa
Leehwa531 <113510252+Leehwa531@users.noreply.github.com>
2022-10-06 05:29:38 +0900

    2nd Week

---
+++ 소스3.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//
+//double temperature(double a);
+//
+//int main()
+//{
+//	
+//	for (double temp = 0; temp <= 100; temp)
+//	{
+//		printf("%.2lf ", temperature(temp));
+//		temp += 0.5;
+//	}
+//
+//
+//
+//}
+//
+//double temperature(double a)
+//{
+//	return (a * 1.8 + 32);
+//}
